6b98d20fcf3fee50fbb38bb1953d7d24be660b0a
hanghouyu <2640738549@qq.com>
2018-12-25 11:51:46 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+	$(".zuojian").click(function () {
+		$(".kaimu1").css("background-image","url(image/2.jpg)")
+	})
+})
+$(".youjian").click(function () {
+	$(".kaimu1").css("background-image","url(image/3.jpg)")
+})
+$(".youjian1").click(function () {
+	$(".wudao2").css("background-image","url(image/3.jpg)")
+})
+$(".zuojian1").click(function () {
+	$(".wudao2").css("background-image","url(image/2.jpg)")
+})
